e9be160492e0d087f7dbcc21dbc2c6ecf67284e5
Ice-hq <igor.rybakov.66@gmail.com>
2024-04-21 18:25:26 +0300

    DelBut

--- src/app/entities/components/table/table.component.ts
+++ src/app/entities/components/table/table.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HeroDataService } from '../services/hero-data.service';
 import { INeedAHero } from '../../interfaces/app.interface';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
@@ -12,8 +13,10 @@ export class TableComponent {
   displayedColumns: string[] = ['name', 'lvl', 'strength', 'skill', 'delete'];
   dataSource = this.tableData;
   constructor(private readonly _hero: HeroDataService) {}
+  @ViewChild(MatTable) public table?: MatTable<INeedAHero>
+
  public onDelete(hero: INeedAHero): void {
   this._hero.deleteHero(hero);
-    // this.table?.renderRows();
+    this.table?.renderRows();
 }
 }
